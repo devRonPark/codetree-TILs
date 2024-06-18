69220ff1ee4124c33803641e55fa1c3f9f78649d
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-18 20:46:49 +0900

    Title: 특정 조건 세 정수 비교, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240618/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 240618/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -11,12 +11,12 @@ public class Main {
         b = sc.nextInt();
         c = sc.nextInt();
 
-        if (a < b && a < c)
+        if (a <= b && a <= c)
             System.out.print("1 ");
         else
             System.out.print("0 ");
 
-        if (a == b && a == c)
+        if (a == b && b == c)
             System.out.println("1");
         else
             System.out.println("0");
