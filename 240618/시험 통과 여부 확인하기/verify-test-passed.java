10c8eb9f5a94d6bbecfe95959d28121b2b3d5528
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-18 11:10:41 +0900

    Title: 시험 통과 여부 확인하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240618/시험 통과 여부 확인하기/verify-test-passed.java
+++ 240618/시험 통과 여부 확인하기/verify-test-passed.java
@@ -8,7 +8,7 @@ public class Main {
 
         if (n >= 80) System.out.println("pass");
         else {
-            System.out.println((n - 80) + " more score");
+            System.out.println((80 - n) + " more score");
         }
     }
 }
