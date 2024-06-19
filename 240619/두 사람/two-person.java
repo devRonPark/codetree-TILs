83fcb1812490e68f6635e192cf7f9a74dc7a13e2
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-19 09:18:59 +0900

    Title: 두 사람, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240619/두 사람/two-person.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int ageOf1 = sc.nextInt();
+        String genderOf1 = sc.next();
+        int ageOf2 = sc.nextInt();
+        String genderOf2 = sc.next();
+
+        // equals() : 문자열의 내용이 같은지 비교
+        // == : 문자열의 참조값(주소값)이 같은지 비교
+        if (genderOf1.equals("M") || genderOf2.equals("M")) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
