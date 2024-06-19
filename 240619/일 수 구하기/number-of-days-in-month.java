c3b5824bb7d1c7a798d22180fd6cb01ef0db95ed
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-19 12:14:39 +0900

    Title: 일 수 구하기, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240619/일 수 구하기/number-of-days-in-month.java
+++ 240619/일 수 구하기/number-of-days-in-month.java
@@ -6,11 +6,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // 2월 => 28일, 짝수 달에 30일, 홀수 달에 31일
         if (n == 2) {
             System.out.println("28");
-        } else if (n % 2 == 0) {
-            System.out.println("30");
         } else {
             System.out.println("31");
         }
