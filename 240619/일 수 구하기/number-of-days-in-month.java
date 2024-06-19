fb7ea6bf9b309dfa20d8141bf03c7e500d4eb801
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-19 12:09:05 +0900

    Title: 일 수 구하기, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240619/일 수 구하기/number-of-days-in-month.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        // 4의 배수라면 => 윤년
+        // 4의 배수가 아니라면 => 평년
+        // 100의 배수이면서 400의 배수가 아니라면 => 평년
+    
+        // 4의 배수 + 100의 배수가 아닌 경우 => 윤년
+        // 4의 배수 + 100의 배수 + 400의 배수인 경우 => 윤년
+        int n = sc.nextInt();
+
+        // 입력값이 평년이라면
+        if (!((n % 4 == 0 && n % 100 == 0) || (n % 4 == 0 && n % 100 == 0 && n % 400 == 0))) {
+            if (n == 2) {
+                System.out.println("28");
+            } else {
+                System.out.println("31");
+            }
+        }
+    }
+}
