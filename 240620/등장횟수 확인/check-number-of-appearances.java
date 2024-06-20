213202195c7a82b2d443bd799e074f6eab0903a2
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-20 13:19:44 +0900

    Title: 등장횟수 확인, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240620/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int n, i;
+        int cntOfEven = 0; // 짝수 등장 횟수
+
+        // 5개의 숫자 입력에 대해,
+        for (i = 1; i <= 5; i++) {
+            n = sc.nextInt();
+            if (i % 2 == 0) {
+                cntOfEven++;
+            }
+        }
+        System.out.print(cntOfEven);
+    }
+}
