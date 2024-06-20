a3a0d2caae345f5aaeeb701310922f86c1ba831b
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-20 11:18:32 +0900

    Title: 짝수만 출력, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240620/짝수만 출력/output-only-even.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, i;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        i = a;
+
+        while (i <= b) {
+            System.out.print(i + " ");
+            i += 2;
+        }
+    }
+}
