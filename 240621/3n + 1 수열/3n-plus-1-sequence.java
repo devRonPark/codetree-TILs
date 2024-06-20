44434bb1bbefa4549781badf9aabf678853fc557
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-21 08:35:47 +0900

    Title: 3n + 1 수열, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240621/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n, cnt = 0; 
+        n = sc.nextInt();
+
+        while (true) {
+            if (n % 2 == 0) {
+                n /= 2;
+            }
+            else {
+                n = (n * 3) + 1;
+            }
+            cnt++;
+
+            if (n == 1) {
+                break;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
