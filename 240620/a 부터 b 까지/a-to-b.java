73838b8d9d6f700ec8894513cdcf30abdf50178b
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-20 12:23:36 +0900

    Title: a 부터 b 까지, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240620/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, i;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        i = a;
+        while (i <= b) {
+            System.out.print(i + " ");
+
+            if (i % 2 == 1) {
+                i *= 2;
+            } else {
+                i += 3;
+            }
+        }
+    }
+}
