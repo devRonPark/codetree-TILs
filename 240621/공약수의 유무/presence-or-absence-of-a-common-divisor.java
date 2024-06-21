72cdf8964413498d28a1a6490206fcadb040084f
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-21 20:21:52 +0900

    Title: 공약수의 유무, Time: 154ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240621/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        boolean satisfied = false;
+
+        for (int i = a; i <= b; i++) {
+            // 1920 이나 2880 을 나눌 수 있는 정수가 하나라도 존재하면 
+            if (1920 % i == 0 || 2880 % i == 0) {
+                satisfied = true;
+                break;
+            }
+        }
+
+        if (satisfied == true) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
