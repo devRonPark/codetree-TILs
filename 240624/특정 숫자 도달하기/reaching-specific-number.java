f29cce44bb418b38da60cdd6ca4c29bb479c2569
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-24 12:02:04 +0900

    Title: 특정 숫자 도달하기, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240624/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int sum = 0, cnt = 0;
+        double avg;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            // 250 이상의 정수라면,
+            if (arr[i] >= 250) {
+                break;
+            }
+            sum += arr[i];
+            cnt++;
+        }
+
+        // 250 이상의 정수가 없다면, cnt 값은 주어진 정수의 개수와 동일
+        if (cnt == 10) {
+            for (int i = 0; i < 10; i++) {
+                sum += arr[i];
+            }
+            avg = (double) sum / 10;
+        }
+        else {
+            avg = (double) sum / cnt;
+        }
+        // 평균은 반올림하여 소수 첫째자리까지
+        String avgStr = String.format("%.1f", avg);
+
+        System.out.print(sum + " " + avgStr);
+    }
+}
