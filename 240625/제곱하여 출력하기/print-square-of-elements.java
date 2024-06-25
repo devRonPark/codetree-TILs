38ca2b3bc15a235e77be026074dfba128fd258ea
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-25 16:13:50 +0900

    Title: 제곱하여 출력하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240625/제곱하여 출력하기/print-square-of-elements.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[100];
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] * arr[i] + " ");
+        }
+
+        
+    }
+}
