acaf38777df8714630f138abeddf8c8e7c2f95bd
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-06-25 15:36:10 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240625/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.java
+++ 240625/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.java
@@ -4,20 +4,26 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-
+        // 변수 선언
         int[] arr = new int[10];
-        int sumOfOddIdx = 0, sumOfEvenIdx = 0;
+
+        int sumOfOddIdx = 0;
+        int sumOfEvenIdx = 0;
+
+        // 10개의 정수를 배열에 입력받아 저장.
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
-
+        }
+        // 홀수번째 수들의 합과 짝수번째 수들의 합 계산
+        for (int i = 0; i < 10; i++) {
             if (i % 2 == 0) {
                 sumOfOddIdx += arr[i];
             }
-            else if (i % 2 == 1) {
+            else {
                 sumOfEvenIdx += arr[i];
             }
         }
-
+        // 비교해 큰 쪽에서 작은 쪽을 뺍니다.
         if (sumOfOddIdx > sumOfEvenIdx) {
             System.out.println(sumOfOddIdx - sumOfEvenIdx);
         }
